8952b7eac0aac90a8cc47bffc57fb872a71e99e5
sravani <sravyadommeti16@gmail.com>
2026-07-17 20:31:15 +0530

    Delete providers.tsx

--- providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { UiProvider } from '@/context/ui-context';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <UiProvider>{children}</UiProvider>;
-}
